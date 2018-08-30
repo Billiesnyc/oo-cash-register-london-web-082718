87cb30063ced62512abbc505bb29dfb6de5a65c8
Billie Simmons <billiesnyc@gmail.com>
2018-08-30 13:22:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,8 +8,8 @@ class CashRegister
     @items = [ ]
   end
   
-  def add_item(title, price, quantity=0)
-    @total += price
+  def add_item(title, price, quantity=1)
+    @total += price * quantity 
   end
 
   
