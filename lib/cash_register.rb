9658f4b38d8cbf4a00c849ae9bed2e59c85b78cc
Billie Simmons <billiesnyc@gmail.com>
2018-08-30 13:29:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,9 +16,9 @@ class CashRegister
     if @discount > 1 
       @discount = ((@discount * @total)/100.00)
       @total -= @discount
-      puts "After the discount, the total comes to $#{@total}."
+      return "After the discount, the total comes to $#{@total}."
     else
-      puts "There is no discount to apply."
+      return "There is no discount to apply."
     end
   end
   
